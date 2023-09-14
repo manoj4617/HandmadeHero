43a07295c890c0e2ed6651029f60012f0081f997
manoj4617 <manojkumarbagewadi4617@gmail.com>
2023-09-14 08:24:23 +0530

    completed build file

--- build.bat
+++ build.bat
@@ -1,2 +1,22 @@
 @echo off
 
+set "BUILD_DIR=.\build"
+
+call .\setup.bat
+set PATH=F:\LEARN\HandmadeHero;%PATH%
+
+
+if not exist "%BUILD_DIR%" (
+    echo Build directory does not. Creating it...
+    mkdir  "%BUILD_DIR%"
+    
+    if errorlevel 1 (
+        echo Failed to create a build directory.
+    ) else (
+        echo Build directory created succeccfully.
+    )
+) else (
+    pushd .\build
+    cl ..\HandmadeHero.cpp
+    popd 
+)
